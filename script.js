25ae12b39e40fccbc2f2e130e1095ff7f5d6e47d
Bryan Leanoe <bryan.leano@gmail.com>
2018-11-11 12:58:41 -0700

    add in calculate age prototype

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ var Person = function(name, yearofBirth, job) {
   this.name = name;
   this.yearofBirth = yearofBirth;
   this.job = job;
+  this.calculateAge = function() {
+    console.log(2018 - yearofBirth);
+  }
+};
+
+Person.prototype.calculateAge = function() {
+  console.log(2018 - yearofBirth);
 };
 
 var john = new Person('John', 1990, 'job');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
