5deb48d3a1beedbd99d07efe87d3e19d12f11566
Bryan Leanoe <bryan.leano@gmail.com>
2018-11-11 18:59:41 -0700

    add primitives v objects demo

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ console.log(mark.lastName);*/
 
 // Object.create
 
-var personProto = {
+/*var personProto = {
   calculateAge: function() {
     console.log(2018 - yearOfBirth);
   }
@@ -50,4 +50,39 @@ var jane = Object.create(personProto, {
   name: { value: 'Jane' },
   yearOfBirth: { value: 1990 },
   job: { value: 'deaigner' }
-});
+});*/
+
+// Primitives vs objects
+
+var a = 23;
+var b = a;
+a = 46;
+
+console.log(a, b);
+
+var obj1 = {
+  name: 'John',
+  age: 26
+};
+
+var obj2 = obj1;
+
+obj1.age = 30;
+
+console.log(obj1.age, obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+  name: 'Jonas',
+  city: 'Lisbon'
+}
+
+function change(a, b) {
+  a = 30;
+  b.city = 'San Francisco'
+};
+
+change(age, obj);
+
+console.log(age, obj.city);
