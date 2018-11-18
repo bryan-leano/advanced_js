e00c1c407fd10c9f9ab5a3e070aa481a4021f77f
Bryan Leanoe <bryan.leano@gmail.com>
2018-11-18 16:50:25 -0700

    add in quiz game

--- script.js
+++ script.js
@@ -217,7 +217,7 @@ interviewQuestion('teacher')('John'); */
 /////////////////////////////////////////////
 ///////// Lecture: Bind, call and apply
 
-var john = {
+/* var john = {
   name: 'John',
   age: 26, 
   job: 'teacher',
@@ -278,4 +278,50 @@ function isFullAge(limit, el) {
 var ages = arrayCalc(years, calculateAge);
 var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 console.log(ages);
-console.log(fullJapan);
+console.log(fullJapan); */
+
+///////////////////////////////////
+////////// Quiz game
+
+(function() {
+function Question(question, answers, correct) {
+  this.question = question;
+  this.answers = answers;
+  this.correct = correct;
+};
+
+Question.prototype.displayQuestion = function() {
+  console.log(this.question);
+
+  for (var i = 0; i < this.answers.length; i ++) {
+    console.log(i + ': ' + this.answers[i]);
+  }
+}
+
+Question.prototype.checkAnswer = function(ans) {
+  if (ans === this.correct) {
+    console.log('Correct answer!');
+  } else {
+    console.log('Wrong answer. Try again :)');
+  }
+}
+
+var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+  ['Yes', 'No'], 0);
+
+var q2 = new Question('What is the name of this course\'s teacher?',
+  ['John', 'Michael', 'Jonas'], 2);
+
+var q3 = new Question('What does best describe coding?',
+  ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
+
+var questions = [q1, q2, q3];
+
+var n = Math.floor(Math.random() * questions.length);
+
+questions[n].displayQuestion();
+
+var answer = parseInt(prompt('Please select the correct answer'));
+
+questions[n].checkAnswer(answer);
+})();
